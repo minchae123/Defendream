51c44743607af259ca12f18c50b46eb203f79902
Inheoseongbin <seongbin529@gmail.com>
2024-01-03 18:37:04 +0900

    스킬 아이콘 넣고 스킬 다 만듬

---
+++ DefenDream/Assets/01_Scripts/Skill/BombSkill.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombSkill : MonoBehaviour
+{
+    private Animator anim;
+    private Rigidbody rb;
+
+    [SerializeField] private GameObject bomb;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+
+        Invoke(nameof(Shaking), 1.5f);
+        rb.AddForce(new Vector3(0,7,0), ForceMode.Impulse);
+    }
+
+    void Shaking()
+    {
+        anim.enabled = true;
+        Invoke(nameof(Bomb), 1.5f);
+    }
+
+    void Bomb()
+    {
+        Instantiate(bomb, null);
+        Destroy(gameObject);
+    }
+}
