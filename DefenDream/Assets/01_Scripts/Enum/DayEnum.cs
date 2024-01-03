40334f8f7d5c785498e2ee95a7417ce14404adcb
ye0jin <fruitfighterlim@gmail.com>
2024-01-03 23:04:42 +0900

    diary

--- DefenDream/Assets/01_Scripts/Enum/DayEnum.cs
+++ DefenDream/Assets/01_Scripts/Enum/DayEnum.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 namespace DayEnum
 {
+    [System.Serializable]
+    public enum WhatDay // 월/화수목금 이런 거... 언제날인지
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday,
+        None
+    }
+    [System.Serializable]
     public enum DayType // 오늘 날이 좋은지 안 좋은지 
     {
         GoodDay,
