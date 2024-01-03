c25e2a5381a5905f52909230692dd7e1701b14d2
Inheoseongbin <seongbin529@gmail.com>
2024-01-03 14:17:57 +0900

    플레이어 스킬 제작++

---
+++ DefenDream/Assets/01_Scripts/Skill/TornadoSkill.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TornadoSkill : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 2.75f);
+    }
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.Euler(-90, 0, transform.rotation.z);
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("Enemy")) 
+    //    {
+    //        other.gameObject.GetComponent<Rigidbody>().AddForce(transform.position - other.transform.position);
+    //    }
+    //}
+}
