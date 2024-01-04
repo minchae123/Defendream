19db1d8bec4dce2971c597c5a4586a1a3df727df
Woo Hyeok <pwh060720@gmail.com>
2024-01-04 19:18:28 +0900

    .

--- DefenDream/Assets/01_Scripts/Player/OurTeam/OurTeam.cs
+++ DefenDream/Assets/01_Scripts/Player/OurTeam/OurTeam.cs
@@ -27,7 +27,6 @@ public class OurTeam : PoolableMono
         _hp = _playerSO._Hp;
         _saveSpeed = _move._speed;
         hpbar.SetHP(_hp);
-        hpbar.ResetHP();
     }
 
     private void Awake()
@@ -64,35 +63,23 @@ public class OurTeam : PoolableMono
 
     private void Magic()
     {
-        PlayerBullet MagicBullet = PoolManager.Instance.Pop("Magic") as PlayerBullet;
-        MagicBullet.transform.position = _FirePos.position;
+        PlayerBullet bullet = Instantiate(_magicBullet, transform);
+        bullet.transform.position = _FirePos.position;
 
-        MagicBullet.GetComponent<Rigidbody>().velocity = _move._direction.normalized * _bulletSpeed;
+        bullet.GetComponent<Rigidbody>().velocity = _move._direction.normalized * _bulletSpeed;
     }
 
     private void Archer()
     {
-        PlayerBullet ArcherBullet = PoolManager.Instance.Pop("Arrow") as PlayerBullet;
+        PlayerBullet bullet = Instantiate(_ArcherBullet, transform);
+        bullet.transform.position = _FirePos.position;
 
-        ArcherBullet.transform.position = _FirePos.position;
-
-        // 방향 설정
-        Vector3 bulletDirection = _move._direction.normalized;
-        ArcherBullet.GetComponent<Rigidbody>().velocity = bulletDirection * _bulletSpeed;
-
-        // 회전 설정
-        if (bulletDirection != Vector3.zero)
-        {
-            Quaternion bulletRotation = Quaternion.LookRotation(bulletDirection, Vector3.up);
-            bulletRotation *= Quaternion.Euler(90f, 0f, 0f);
-            ArcherBullet.GetComponent<Rigidbody>().rotation = bulletRotation;
-        }
+        bullet.GetComponent<Rigidbody>().velocity = _move._direction.normalized * _bulletSpeed;
     }
 
     public void DecHp(float damage)
     {
         _hp -= damage;
-        _hp = Mathf.Clamp(_hp, 0, _playerSO._Hp);
         hpbar.OnDamage(damage);
         DieAnim();
 
